899fabb87b43e53d4d3437b3f87f48e5dff28ba0
Linus Bierwirth <lm.bierwirth@gmail.com>
2022-03-30 17:27:43 -0400

    Lansing event code

---
+++ src/main/java/frc/robot/subsystems/wpilibSwerveDrive.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class wpilibSwerveDrive extends SubsystemBase {
+  /** Creates a new wpilibSwerveDrive. */
+  public wpilibSwerveDrive() {
+
+    // private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+    //   // Front left
+    //   new Translation2d(Constants.ROBOT_LENGTH / 2, Constants.ROBOT_WIDTH / 2),
+    //   // Front right
+    //   new Translation2d(Constants.ROBOT_LENGTH / 2, Constants.ROBOT_WIDTH / 2),
+    //   // Front left
+    //   new Translation2d(Constants.ROBOT_LENGTH / 2, Constants.ROBOT_WIDTH / 2),
+    //   // Front right
+    //   new Translation2d(Constants.ROBOT_LENGTH / 2, Constants.ROBOT_WIDTH / 2)
+    // );
+
+    // private final SwerveModule m_frontLeftModule;
+    // private final SwerveModule m_frontRightModule;
+    // private final SwerveModule m_backLeftModule;
+    // private final SwerveModule m_backRightModule;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
